6eebc1394aec394a3a3531d2b6b2ae35b60bbc8e
Vasili Maslov <bassill@yandex.ru>
2013-05-18 23:26:53 +0400

    JSON serialization

--- SchemeEditor/Schema/Room.cs
+++ SchemeEditor/Schema/Room.cs
@@ -5,14 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
+using Newtonsoft.Json;
 
 namespace SchemeEditor.Schema
 {
-    public class Room
+    public class Room : IJsonObject
     {
         public string Title { get; set; }
         public string Id { get; set; }
         public Color Color { get; set; }
         public Point[] Points { get; set; }
+
+        public string GetHexColor()
+        {
+            return "#" + Color.R.ToString("X2") + Color.G.ToString("X2") + Color.B.ToString("X2");
+        }
+
+        public Dictionary<string, object> ToJsonObject()
+        {
+            return new Dictionary<string, object>
+            {
+                { "title", Title },
+                { "id", Id },
+                { "color", GetHexColor() },
+                { "points", Points.Select(p => new[] { p.X, p.Y }) }
+            };
+        }
     }
 }
