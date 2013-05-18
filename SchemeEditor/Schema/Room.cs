11a9e04c24f358e3fec0bf2965eec7ed98f76a46
Vasili Maslov <bassill@yandex.ru>
2013-05-18 20:11:07 +0400

    Room

---
+++ SchemeEditor/Schema/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace SchemeEditor.Schema
+{
+    class Room
+    {
+        public string Title;
+        public string Id;
+        public Color Color;
+        public Point[] Points;
+    }
+}
